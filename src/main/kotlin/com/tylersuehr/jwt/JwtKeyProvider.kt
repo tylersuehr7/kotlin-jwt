dc8e6be7304dc471fb68ad3f849fcf1e5c503bf0
Tyler Suehr <tylersuehr@gmail.com>
2021-06-14 20:10:32 -0400

    Defined a cryptographic key provider.

---
+++ src/main/kotlin/com/tylersuehr/jwt/JwtKeyProvider.kt
@@ -0,0 +1,12 @@
+package com.tylersuehr.jwt
+
+import java.security.Key
+
+/**
+ * Defines a provider for token cryptographic keys.
+ * @author Tyler Suehr
+ */
+interface JwtKeyProvider {
+    fun getSignKey(): Key
+    fun getVerifyKey(): Key
+}
